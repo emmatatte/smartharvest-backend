71a909a897e1600180b6f45c36c5c29c1d33dfff
John <johnleonmelgarejo@gmail.com>
2025-04-23 10:42:35 -0500

    Subiendo el proyecto

---
+++ src/main/java/pe/edu/upc/smartharvest/SmartHarvestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.smartharvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHarvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHarvestApplication.class, args);
+	}
+
+}
