73d1f3c6eb10e45807b21bdc0c3f11156820f0c1
John <johnleonmelgarejo@gmail.com>
2025-04-27 13:31:48 -0500

    CRUD de Tabla Sensor Completo

---
+++ src/main/java/pe/edu/upc/smartharvest/controllers/SensorController.java
@@ -0,0 +1,51 @@
+package pe.edu.upc.smartharvest.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.smartharvest.dtos.SensorDTO;
+import pe.edu.upc.smartharvest.dtos.UsuarioDTO;
+import pe.edu.upc.smartharvest.entities.Sensor;
+import pe.edu.upc.smartharvest.entities.Usuario;
+import pe.edu.upc.smartharvest.servicesinterfaces.IRolService;
+import pe.edu.upc.smartharvest.servicesinterfaces.ISensorService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/sensores")
+public class SensorController {
+    @Autowired
+    private ISensorService sS;
+
+    public SensorController() {
+    }
+
+    @GetMapping
+    public List<SensorDTO> listar() {
+        return sS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, SensorDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void registrar(@RequestBody SensorDTO sDTO) {
+        ModelMapper m = new ModelMapper();
+        Sensor s = m.map(sDTO, Sensor.class);
+        sS.insert(s);
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody SensorDTO sDTO) {
+        ModelMapper m = new ModelMapper();
+        Sensor s = m.map(sDTO, Sensor.class);
+        sS.update(s);
+    }
+
+    @DeleteMapping("/{idSensor}")
+    public void eliminar(@PathVariable int idSensor) {
+        sS.delete(idSensor);
+    }
+}
