16138118394f2007e68a98e40abea2d02ffbd0c1
brayan250-cpu <brayanguizado5@gmail.com>
2025-04-30 07:55:38 -0500

    Update SensorDTO.java

--- src/main/java/pe/edu/upc/smartharvest/dtos/SensorDTO.java
+++ src/main/java/pe/edu/upc/smartharvest/dtos/SensorDTO.java
@@ -1,10 +1,5 @@
 package pe.edu.upc.smartharvest.dtos;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import pe.edu.upc.smartharvest.entities.Parcel;
-
 import java.time.LocalDate;
 
 public class SensorDTO {
@@ -14,4 +9,65 @@ public class SensorDTO {
     private boolean state;
     private LocalDate lastLecture;
     private Parcel parcel;
+
+    public SensorDTO() {
+    }
+
+    public SensorDTO(int idSensor, String sensorType, LocalDate installationDate, boolean state, LocalDate lastLecture, Parcel parcel) {
+        this.idSensor = idSensor;
+        this.sensorType = sensorType;
+        this.installationDate = installationDate;
+        this.state = state;
+        this.lastLecture = lastLecture;
+        this.parcel = parcel;
+    }
+
+    public int getIdSensor() {
+        return idSensor;
+    }
+
+    public void setIdSensor(int idSensor) {
+        this.idSensor = idSensor;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
+
+    public LocalDate getInstallationDate() {
+        return installationDate;
+    }
+
+    public void setInstallationDate(LocalDate installationDate) {
+        this.installationDate = installationDate;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public LocalDate getLastLecture() {
+        return lastLecture;
+    }
+
+    public void setLastLecture(LocalDate lastLecture) {
+        this.lastLecture = lastLecture;
+    }
+
+    public Parcel getParcel() {
+        return parcel;
+    }
+
+    public void setParcel(Parcel parcel) {
+        this.parcel = parcel;
+    }
 }
+
