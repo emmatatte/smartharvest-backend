cf5fcca635f127d5ac2735959c7b46674647f99a
brayan250-cpu <brayanguizado5@gmail.com>
2025-04-30 07:36:27 -0500

    Update ISensorRepository.java

--- src/main/java/pe/edu/upc/smartharvest/repositories/ISensorRepository.java
+++ src/main/java/pe/edu/upc/smartharvest/repositories/ISensorRepository.java
@@ -4,4 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.upc.smartharvest.entities.Sensor;
 
 public interface ISensorRepository extends JpaRepository<Sensor, Integer> {
+  @Query("SELECT s FROM Sensor s WHERE s.parcel.id = :idParcela AND s.fecha BETWEEN :inicio AND :fin")
+    List<Sensor> obtenerDatosSatelitalesPorParcela(
+      @Param("idParcela") Long idParcela,
+      @Param("inicio") LocalDate inicio,
+      @Param("fin") LocalDate fin
+      );
 }
