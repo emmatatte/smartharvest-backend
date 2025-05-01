4c1a9972d0deabae55d91e667f1bf6225aad8010
SebaasPacheco <sebas_4235@outlook.com>
2025-04-30 20:44:11 -0500

    Implementacion de getter y setter esn CropDTO

--- src/main/java/pe/edu/upc/smartharvest/dtos/CropDTO.java
+++ src/main/java/pe/edu/upc/smartharvest/dtos/CropDTO.java
@@ -12,4 +12,52 @@ public class CropDTO {
     private String estimatedHarvestDate;
     private String actualState;
     private Parcel parcel;
+
+    public int getIdCrop() {
+        return idCrop;
+    }
+
+    public void setIdCrop(int idCrop) {
+        this.idCrop = idCrop;
+    }
+
+    public String getTypeCrop() {
+        return typeCrop;
+    }
+
+    public void setTypeCrop(String typeCrop) {
+        this.typeCrop = typeCrop;
+    }
+
+    public String getSowingDate() {
+        return sowingDate;
+    }
+
+    public void setSowingDate(String sowingDate) {
+        this.sowingDate = sowingDate;
+    }
+
+    public String getEstimatedHarvestDate() {
+        return estimatedHarvestDate;
+    }
+
+    public void setEstimatedHarvestDate(String estimatedHarvestDate) {
+        this.estimatedHarvestDate = estimatedHarvestDate;
+    }
+
+    public String getActualState() {
+        return actualState;
+    }
+
+    public void setActualState(String actualState) {
+        this.actualState = actualState;
+    }
+
+    public Parcel getParcel() {
+        return parcel;
+    }
+
+    public void setParcel(Parcel parcel) {
+        this.parcel = parcel;
+    }
 }
