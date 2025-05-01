e5ae150f7c2b51d9cbf1742cf9c3289f675d93ac
SebaasPacheco <sebas_4235@outlook.com>
2025-04-30 20:37:17 -0500

    Implementacion de getter y setter para CompanyDTO

--- src/main/java/pe/edu/upc/smartharvest/dtos/CompanyDTO.java
+++ src/main/java/pe/edu/upc/smartharvest/dtos/CompanyDTO.java
@@ -12,5 +12,67 @@ public class CompanyDTO {
     private String email;
     private String contact;
 
+    public int getIdCompany() {
+        return idCompany;
+    }
 
+    public void setIdCompany(int idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMission() {
+        return mission;
+    }
+
+    public void setMission(String mission) {
+        this.mission = mission;
+    }
+
+    public String getVision() {
+        return vision;
+    }
+
+    public void setVision(String vision) {
+        this.vision = vision;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }
