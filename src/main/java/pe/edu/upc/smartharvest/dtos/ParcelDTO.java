c36e933bf44f6cba8c1f5214b78f71164a2e00e7
John <johnleonmelgarejo@gmail.com>
2025-05-01 15:44:39 -0500

    Se agrego getter y setter faltantes de DTOS

--- src/main/java/pe/edu/upc/smartharvest/dtos/ParcelDTO.java
+++ src/main/java/pe/edu/upc/smartharvest/dtos/ParcelDTO.java
@@ -12,4 +12,60 @@ public class ParcelDTO {
     private String groundType;
     private LocalDate registrationDate;
     private Users users;
+
+    public int getIdParcel() {
+        return idParcel;
+    }
+
+    public void setIdParcel(int idParcel) {
+        this.idParcel = idParcel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getSizem2() {
+        return sizem2;
+    }
+
+    public void setSizem2(double sizem2) {
+        this.sizem2 = sizem2;
+    }
+
+    public String getGroundType() {
+        return groundType;
+    }
+
+    public void setGroundType(String groundType) {
+        this.groundType = groundType;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
 }
